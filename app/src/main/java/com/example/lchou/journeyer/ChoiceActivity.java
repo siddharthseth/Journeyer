2af53feac90ef055fdfaa6b45e0b59d4d6ac9caa
Siddharth Seth <siddharth.seth@berkeley.edu>
2015-10-11 00:25:01 -0700

    Updated host frame

--- app/src/main/java/com/example/lchou/journeyer/ChoiceActivity.java
+++ app/src/main/java/com/example/lchou/journeyer/ChoiceActivity.java
@@ -2,12 +2,32 @@ package com.example.lchou.journeyer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.content.Intent;
 
-public class ChoiceActivity extends AppCompatActivity {
+public class ChoiceActivity extends AppCompatActivity implements View.OnClickListener
+{
+    ImageButton hostButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choice);
+        hostButton =(ImageButton)findViewById(R.id.hostButton);
+        hostButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view){
+        switch (view.getId()) {
+            case R.id.hostButton:
+                try {
+                    startActivity(new Intent(getApplicationContext(), MapsActivity.class));
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                break;
+        }
     }
 }
--- app/src/main/java/com/example/lchou/journeyer/MapsActivity.java
+++ app/src/main/java/com/example/lchou/journeyer/MapsActivity.java
@@ -2,17 +2,22 @@ package com.example.lchou.journeyer;
 
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    boolean foodBoolean = false;
+    boolean shelterBoolean = false;
+    boolean waterBoolean = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,13 +39,66 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
+
+    public void buttonFood(View v) {
+        foodBoolean = !foodBoolean;
+        v.setActivated(foodBoolean);
+        LatLng r8 = new LatLng(40.01, 33.03);
+        mMap.addMarker(new MarkerOptions().position(r8).title("TESTING")
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_MAGENTA)));
+    }
+
+    public void buttonWater(View v) {
+        waterBoolean = !waterBoolean;
+        v.setActivated(waterBoolean);
+    }
+
+    public void buttonShelter(View v) {
+        shelterBoolean = !shelterBoolean;
+        v.setActivated(shelterBoolean);
+
+    }
+
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        // Add a marker in Ankara (represents the host) and move the camera
+        LatLng ankara = new LatLng(40, 33);
+        MarkerOptions cur = new MarkerOptions().position(ankara).title("You");
+        mMap.addMarker(cur);
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(ankara, 12));
+
+
+        // Add a refugee marker
+        LatLng r1 = new LatLng(40, 33.02);
+        mMap.addMarker(new MarkerOptions().position(r1).title("Refugee 1")
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));
+
+        // Add a refugee marker
+        LatLng r2 = new LatLng(40.02, 33.01);
+        mMap.addMarker(new MarkerOptions().position(r2).title("Refugee 2")
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));
+
+        // Add a refugee marker
+        LatLng r3 = new LatLng(40.005, 32.98);
+        mMap.addMarker(new MarkerOptions().position(r3).title("Refugee 3")
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));
+
+        // Add a refugee marker
+        LatLng r4 = new LatLng(39.98, 33);
+        mMap.addMarker(new MarkerOptions().position(r4).title("Refugee 4")
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));
+
+        // Add a refugee marker
+        LatLng r5 = new LatLng(40.01, 33.02);
+        mMap.addMarker(new MarkerOptions().position(r5).title("Refugee 5")
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));
+
+        // Add a refugee marker
+        LatLng r6 = new LatLng(40.01, 33.01);
+        mMap.addMarker(new MarkerOptions().position(r6).title("Refugee 6")
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));
     }
 }
